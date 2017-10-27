2ba77621baf4a065406258776108035b6f10a933
gnomengineer <troopa.der.pate@gmail.com>
2017-10-27 15:05:48 +0200

    enhanced main to quit properly (^c still break code)

--- src/mainTest.cpp
+++ src/mainTest.cpp
@@ -4,12 +4,15 @@
 
 int main( int argc, const char* argv[] )
 {
+    int c;
 	GPIO pin2("27");
     pin2.setDirection(GPIO::INPUT);
     GPIO pin3("17");
     pin3.setDirection(GPIO::OUTPUT);
     pin3.setValue(GPIO::HIGH);
 
-    while (1)
-        std::cout << pin2.readValue();
+    do {
+        std::cout << pin2.readValue() << std::endl;
+        c = getchar();
+    } while (c != 'q');
 }
