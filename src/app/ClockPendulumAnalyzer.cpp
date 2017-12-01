32eed83792fe791cc8d62ffbcac381bc54ee6330
gnomengineer <troopa.der.pate@gmail.com>
2017-12-01 12:22:44 +0100

    testoutput from the db

--- src/app/ClockPendulumAnalyzer.cpp
+++ src/app/ClockPendulumAnalyzer.cpp
@@ -26,6 +26,8 @@ void ClockPendulumAnalyzer::startAnalyze() {
         //TODO use DataAssembler.getNewDataSample and print it to cout
         std::cout << m_DataList.back().getClockName() << " in DataTupel" << std::endl;
 
+        
+        std::cout << m_DataTransfer.getDataListByName(m_ClockName).back().getClockName() << std::endl;
         std::this_thread::sleep_for(timespan);
     }
 }
