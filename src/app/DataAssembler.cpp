a1044b40ee1ff9c11494dad4c55d1bea040af486
gnomengineer <troopa.der.pate@gmail.com>
2017-12-15 13:32:29 +0100

    added UART Handler and Receiver from PREN39 project of tkreienbühl

--- src/app/DataAssembler.cpp
+++ src/app/DataAssembler.cpp
@@ -3,7 +3,14 @@
 #include <iostream>
 
 DataTupel DataAssembler::getNewDataSample(std::string clockname) {
-    int absoluteTime = m_Analyzer.getTimeDifference_ns();
+    
+
+    m_time_list_mutex.lock();
+    m_time_list.front();
+    m_time_list.pop_front();
+    m_time_list_mutex.unlock();
+
+    int absoluteTime = 1; //TODO get absolute time from list
     time_t now = time(0);
     struct tm* localTime = localtime(&now);
     //TODO fix time return values
@@ -13,4 +20,11 @@ DataTupel DataAssembler::getNewDataSample(std::string clockname) {
 
     DataTupel dataSample(clockname,"00000000",absoluteTime);
     return dataSample;
+}
+
+void DataAssembler::addToVector(std::string timeKey, int absoluteTimeValue) {
+    std::pair<std::string, int> pair(timeKey, absoluteTimeValue);
+    m_time_list_mutex.lock();
+    m_time_list.push_back(pair);
+    m_time_list_mutex.unlock();
 }
