a80921ec50249242f5df753797f1857e3c013615
Tobias Kreienbühl <tkreienbuehl@bluewin.ch>
2018-01-02 14:00:38 +0100

    Message parsing changed to two values

--- src/app/UARTReceiver.cpp
+++ src/app/UARTReceiver.cpp
@@ -30,7 +30,7 @@ void UARTReceiver::readAndPlotData() {
 			//----- CHECK FOR ANY RX BYTES -----
 		if (m_uart0_filestream != -1) {
 			bzero(rx_buffer,sizeof(rx_buffer));
-			int rx_length = read(m_uart0_filestream, rx_buffer, sizeof(rx_buffer));
+			ssize_t rx_length = read(m_uart0_filestream, rx_buffer, sizeof(rx_buffer));
 			if (rx_length > 0 ) {
 				string str(reinterpret_cast<const char*>(rx_buffer));
 				if (str.length() > 0) {
@@ -44,8 +44,16 @@ void UARTReceiver::readAndPlotData() {
 void UARTReceiver::decodeRecievedString(std::string message) {
     std::time_t time = std::time(0);
     std::stringstream timeString;
+	std::string pendulumTime, referenceFrequency;
     timeString << std::put_time(std::localtime(&time),"%Y%m%d%H%M%S");
-    m_Observer->addToVector(timeString.str(),std::stoi( message ));
+	pendulumTime = message.substr(0, message.find(';'));
+    auto pendTime = static_cast<uint32_t>(std::stoul(pendulumTime));
+	referenceFrequency = message.substr(message.find(';') + 1);
+	double corrVal = std::stoi(referenceFrequency) / 12000000.0;
+	if (abs(corrVal) > 0.8 && abs(corrVal) < 1.5) {
+		pendTime = static_cast<uint32_t>(pendTime * corrVal);
+	}
+    m_Observer->addToVector(timeString.str(), pendTime);
 }
 
 void* UARTReceiver::staticEntryPoint(void* threadId) {
