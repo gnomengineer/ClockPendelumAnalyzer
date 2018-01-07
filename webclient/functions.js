3ac2c61a580611e1d4724074c31ca7ee27191de7
gnomengineer <troopa.der.pate@gmail.com>
2018-01-07 17:45:59 +0100

    added webclient folder to project

---
+++ webclient/functions.js
@@ -0,0 +1,25 @@
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function displayResponse(responseText)
+{
+    console.log(responseText);
+}
+
+
